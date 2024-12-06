aba968a497ccc85b94146e8923880c20d0dcc4e9
saket <saketgoswami4141@gmail.com>
2024-12-06 15:28:12 +0530

    Initial commit

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -2,9 +2,13 @@ const express = require("express")
 
 const router = express.Router();
 
+const URL = require("../models/url")
 
-router.get("/",  (req, res) => {
-  return res.render("home")
+router.get("/", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls
+  })
 })
 
 module.exports = router;
